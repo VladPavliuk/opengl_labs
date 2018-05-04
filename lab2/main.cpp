fec9df5532c9ca894191c299cd80b38d40fde9a8
vlad <pavliuk.vlad@gmail.com>
2018-05-04 13:44:40 +0300

    50 % made lab 2

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,71 +1,106 @@
 ﻿#include <GL/glut.h>
 #include <math.h>
+#include <iostream>
 
 //------------------КОНСТАНТИ ТА ГЛОБАЛЬНІ ЗМІННІ-------------------------
 
+GLfloat xRotated, yRotated, zRotated;
 int Width = 800, Height = 800; //парметри вікна виводу
 const double Pi = 3.1415926535897932384;
 const double e = 2.718;
 
 //Управління виводом на екран!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+class Sphere 
+{
+	struct Point
+	{
+		double x;
+		double y;
+		double z;		
+	};
+
+	private: 
+		int radius, u, v;
+		const static int m = 50;
+		const static int n = 50;
+
+		Point polygonNet [n][m];
+		
+	public:
+		Sphere(int radius)
+		{
+			this->radius = 2;
+			this->v = Pi * 2;
+			this->u = Pi;
+		}
+
+		void draw() 
+		{
+			for(int i = 0; i < Sphere::n; i++) {
+				for(int j = 0; j < Sphere::n; j++) {
+					glColor3d(1, 0, 1);
+					glBegin(GL_LINES);
+					glVertex3f(
+						this->polygonNet[i][j].x, 
+						this->polygonNet[i][j].y, 
+						this->polygonNet[i][j].z
+					);
+
+					glVertex3f(
+						this->polygonNet[i][j + 1].x, 
+						this->polygonNet[i][j + 1].y, 
+						this->polygonNet[i][j + 1].z
+					);
+					glEnd();
+				 }
+			}	
+		}
+
+		void drawToConsole()
+		{
+			for(int i = 0; i < Sphere::n; i++) {
+				for(int j = 0; j < Sphere::n; j++) {
+					std::cout << "X: " << this->polygonNet[i][j].x << std::endl;
+					std::cout << "Y: " << this->polygonNet[i][j].y << std::endl;
+					std::cout << "Z: " << this->polygonNet[i][j].z << std::endl << std::endl;
+				 }
+			}	
+		}
+
+		void definePolygonNet()
+		{
+			for(int i = 0; (double) this->v / (double) Sphere::m * i < this->v; i++) {
+				for(int j = 0; (double) this->u / (double) Sphere::n * j < this->u; j++) {
+					this->polygonNet[i][j].x = this->radius * sin(j) * cos(i);
+					this->polygonNet[i][j].y = this->radius * sin(j) * cos(i); 
+					this->polygonNet[i][j].z = this->radius * cos(j);
+				 }
+			}
+		}
+				
+};
+
 
 void DrawSphere() {
-	/*const double a = 0.5;
-	const double b = 0.8;
-	const double c = 8;
-
-	double x = a*(sin(c*t)+b)*cos(t);
-	double y = a * (sin(c*t) + b)*sin(t);
-	double z = a * cos(c*t);
-	while (t <= 2*Pi)
-	{
-		double xpred = x;
-		double ypred = y;
-		double zpred = z;
-		x = a * (sin(c*t) + b)*cos(t);
-		y = a * (sin(c*t) + b)*sin(t);
-		z = a * cos(c*t);
-		t += 0.05;
-
-		glColor3d(1, 0, 1);
-		glBegin(GL_LINES);
-		glVertex3f(xpred, ypred, zpred); glVertex3f(x, y, z);
-		glEnd();
-	};*/
-
-	double r = 2, i = 0, j = 0;
-	double m = 10;
-	double n = 10;
-	double x = r * sin(j) * cos(i);
-	double y = r * sin(j) * cos(i); 
-	double z = r * cos(j);
-
-	while(i <= 2 * Pi) {
-		while(j <= Pi) {
-            glColor3d(1, 0, 1);
-            glBegin(GL_LINES);
-			glVertex3f(x, y, z);
-
-			x = r * sin(j) * cos(i);
-			y = r * sin(j) * cos(i); 
-			z = r * cos(j);
-
-            glVertex3f(x, y, z);
-			glEnd();
-			
-			j += Pi / n;
-		 }
-
-		i += 2 * Pi / m;
-	}
+	Sphere* sphere = new Sphere(2);
+	sphere->definePolygonNet();
+	sphere->draw();
+	sphere->drawToConsole();
 };
 
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	//=========================================================	
 
-	glRotated(10, 2, 1, 0);
+	//glRotated(10, 2, 1, 0);
+
+
+	glRotatef(xRotated,1.0,0.0,0.0);
+    // rotation about Y axis
+    glRotatef(yRotated,0.0,1.0,0.0);
+    // rotation about Z axis
+    glRotatef(zRotated,0.0,0.0,1.0);
 
 	//координатні осі
 	glLineWidth(2);
@@ -86,11 +121,9 @@ void display(void) {
 
 	DrawSphere();
 
-	
-
-
 
 							   //=========================================================
+	glFlush(); 
 	glutSwapBuffers();
 }
 
@@ -117,6 +150,7 @@ void scene(void)
 	glEnable(GL_DEPTH_TEST);//режим при якому видаляються невидимі лінії та поверхні
 
 
+
 	glMatrixMode(GL_PROJECTION);
 	glMatrixMode(GL_MODELVIEW);
 	glOrtho(-5, 5, -5, 5, 2, 15);//ортографічна проекція
@@ -127,15 +161,25 @@ void scene(void)
 
 }
 
+void idleFunc(void)
+{
+    /*xRotated += 0.3;
+    yRotated += 0.3;
+    zRotated += 0.3;*/
+    display();
+}
+
 
 //Ініціалізація  GLUT-додатку!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 int main(int argc, char **argv)
 {
+	xRotated = yRotated = zRotated = 0.1;
 	glutInit(&argc, argv); //ф-я здійснює необхіжні початкові значення для побудови вікна додатку
 	glutInitWindowPosition(0, 0); //положення вікна
 	glutInitWindowSize(Width, Height); //розмір вікна
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);// режими відображення інформації (вікно з: буфером глибини, подвійним буфером, режим RGB роботи з кольором)
 	glutCreateWindow("camera"); //назва 
+	glutIdleFunc(idleFunc);
 	glClearColor(0, 0, 0, 0);//колір фону
 
 	glutDisplayFunc(display); // вказується що малювати у вікні додатку при кожній  перемальовці вікні
